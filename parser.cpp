976583b444a9466fb020db58a2903599b47a4ba7
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2021-03-06 12:57:44 +0200

    Implemented minimal lexical analyser

--- parser.cpp
+++ parser.cpp
@@ -1,26 +1,65 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
+int keywordCheck(char *buffer)
+{
+    char keywords[32][10] = {"auto","break","case","char","const","continue","default",
+							"do","double","else","enum","extern","float","for","goto",
+							"if","int","long","register","return","short","signed",
+							"sizeof","static","struct","switch","typedef","union",
+							"unsigned","void","volatile","while"};
+
+    for(int i = 0; i < 32; i++) 
+    {
+        if(strcmp(keywords[i], buffer) == 0)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
-    ifstream f(argv[1]);
+    ifstream fin(argv[1]);
 
-    char buffer[100];
+    char buffer[15];
 
     char c;
-    int nr = -1;
-    while(f >> c)
-    {
-        buffer[++nr] = c;
-    }
+    char operators[] = "+-*/%=";
 
-    for (int i = 0; i <= nr; i++)
-    {
-        cout << buffer[i];
-    }
+    int j = 0;
 
-    f.close();
+    while(!fin.eof()){
+   		c = fin.get();
+   		
+		for(int i = 0; i < 6; ++i){
+   			if(c == operators[i])
+   				cout << c << " is operator\n";
+   		}
+   		
+   		if(isalnum(c)){
+   			buffer[j++] = c;
+   		}
+   		else if((c == ' ' || c == '\n') && (j != 0)){
+   				buffer[j] = '\0';
+   				j = 0;
+   				   				
+   				if(keywordCheck(buffer) == 1)
+   					cout << buffer << " is keyword\n";
+   				else
+   					cout << buffer << " is identifier\n";
+   		}
+   		
+	}
+
+    char aux;
+    cin >> aux;
+
+    fin.close();
     return 0;
 }
--- test.c
+++ test.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 int main() 
 {
     int a = 556;
-
+    
     return 0;
 }
